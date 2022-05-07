cd1e399df1de87d2c3519c75bbc70236287bc0d9
Varga Péter <peter.varga.jdk@gmail.com>
2022-05-07 13:44:21 +0200

    Renaming

--- src/test/java/hu/ak/generics/webapp/TestDemo.java
+++ src/test/java/hu/ak/generics/webapp/TestDemo.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-public class DemoTest {
+public class TestDemo {
 
 	@Test
 	public void testSuccess() {
