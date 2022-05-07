dfb6b93254becbfe5ee77fb05fc2ee7868581371
Varga Péter <peter.varga.jdk@gmail.com>
2022-05-07 11:18:18 +0200

    Sample test added

---
+++ src/test/java/hu/ak/generics/webapp/DemoTest.java
@@ -0,0 +1,20 @@
+package hu.ak.generics.webapp;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class DemoTest {
+
+	@Test
+	public void testSuccess() {
+		assertTrue(3 == 3);
+	}
+	
+	@Test
+	public void testFailure() {
+		assertTrue(2 == 2);
+	}
+	
+}
